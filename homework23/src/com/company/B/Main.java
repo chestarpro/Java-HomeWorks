2445fb7afae0c87f0a148c6e159b664fbf019d22
chestarpro <chestarpro@gmail.com>
2021-05-18 19:27:00 +0600

    add comment

--- homework23/src/com/company/B/Main.java
+++ homework23/src/com/company/B/Main.java
@@ -1,6 +1,4 @@
-package com.company;
-
-import com.company.B.Test;
+package com.company.B;
 
 import javax.swing.*;
 
--- homework23/src/com/company/B/Test.java
+++ homework23/src/com/company/B/Test.java
@@ -13,20 +13,24 @@ public class Test extends  Canvas {
         CubicCurve2D c = new CubicCurve2D.Double ();
 
         final Parabola [] parabola = new Parabola[2];
-        parabola[0] = new Parabola(30);
+        parabola[0] = new Parabola(30); //Формула в классе Parabola
         parabola[1] = new Parabola(200, 480, 180, 10);
-
 
         for (int i = 0; i < parabola.length; i++) {
             if (i == 0) {
                 q.setCurve (5, 0, parabola[i].getX1(), parabola[i].getY1(), 55, 0);
                 System.out.println("x: " + parabola[i].getX1());
                 System.out.println("y: " + parabola[i].getY1());
+                System.out.println();
                 g2.draw (q);
-            } else  c.setCurve (250, 0, parabola[i].getX1(), parabola[i].getY1(), parabola[i].getX2(), parabola[i].getY2(), 130, 500);
-            System.out.println("x: " + parabola[i].getX1() + ", " + parabola[i].getX2());
-            System.out.println("y: " + parabola[i].getY1() + ", " + parabola[i].getY2());
-            g2.draw (c);
+            } else if (i == 1) {
+                c.setCurve (250, 0, parabola[i].getX1(), parabola[i].getY1(),
+                        parabola[i].getX2(), parabola[i].getY2(), 130, 500);
+                System.out.println("x: " + parabola[i].getX1() + ", " + parabola[i].getX2());
+                System.out.println("y: " + parabola[i].getY1() + ", " + parabola[i].getY2()); // Здесь не по формуле.
+                System.out.println(); // я бы мог запарится по искать но там где я нашел совсем уровень другой, ты бы сразу понял что я скоприовал:)
+                g2.draw (c);
+            }
         }
     }
 }
